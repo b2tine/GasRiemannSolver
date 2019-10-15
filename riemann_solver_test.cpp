3b2876b6417e3b308a7dcf99ecb2698c1acc7e17
Brandon <brandon.ballentine@stonybrook.edu>
2019-10-15 13:15:46 -0400

    full x-t riemann solution -- now refactor

--- riemann_solver_test.cpp
+++ riemann_solver_test.cpp
@@ -15,7 +15,7 @@ double LeftCenteredWave(double Pslip, STATE* sl, STATE* sl_center)
 {
     if (Pslip > sl->p)
     {
-        //LCW is Left Facing Shock Wave
+        //LCW is Left Facing Shock (LFS) Wave
         double ua = sl->u;
         double rhoa = sl->rho;
         double pa = sl->p;
@@ -23,6 +23,7 @@ double LeftCenteredWave(double Pslip, STATE* sl, STATE* sl_center)
 
         double taua = 1.0/rhoa;
         double taub = behind_state_specific_volume(rhoa,pa,pb);
+        double rhob = 1.0/taub;
         
         //M > 0 this case
         double M = std::sqrt((pb - pa)/(taua - taub));
@@ -30,18 +31,19 @@ double LeftCenteredWave(double Pslip, STATE* sl, STATE* sl_center)
         
         //save center state variables
         sl_center->u = ub;
-        sl_center->rho = 1.0/taub;
+        sl_center->rho = rhob;
         sl_center->p = pb;
+        sl_center->a = constant_state_soundspeed(rhob,pb);
 
         return ub;
     }
     else
     {
-        //LCW is a S+ Simple Wave
+        //LCW is a GAMMA PLUS (S+) Simple Wave
         double u0 = sl->u;
         double rho0 = sl->rho;
         double p0 = sl->p;
-        double a0 = constant_state_soundspeed(rho0,p0);
+        double a0 = sl->a;
 
         //near slip line state variables
         double p1 = Pslip;
@@ -53,6 +55,7 @@ double LeftCenteredWave(double Pslip, STATE* sl, STATE* sl_center)
         sl_center->u = u1;
         sl_center->rho = rho1;
         sl_center->p = p1;
+        sl_center->a = a1;
 
         return u1;
     }
@@ -62,11 +65,11 @@ double RightCenteredWave(double Pslip, STATE* sr_center, STATE* sr)
 {
     if (Pslip <= sr->p)
     {
-        //RCW is a S- Simple Wave
+        //RCW is a GAMMA MINUS (S-) Simple Wave
         double u0 = sr->u;
         double rho0 = sr->rho;
         double p0 = sr->p;
-        double a0 = constant_state_soundspeed(rho0,p0);
+        double a0 = sr->a;
 
         //near slip line state variables
         double p1 = Pslip;
@@ -78,12 +81,13 @@ double RightCenteredWave(double Pslip, STATE* sr_center, STATE* sr)
         sr_center->u = u1;
         sr_center->rho = rho1;
         sr_center->p = p1;
+        sr_center->a = a1;
 
         return u1;
     }
     else
     {
-        //RCW is a Right Facing Shock Wave
+        //RCW is a Right Facing Shock (RFS) Wave
         double ua = sr->u;
         double rhoa = sr->rho;
         double pa = sr->p;
@@ -91,6 +95,7 @@ double RightCenteredWave(double Pslip, STATE* sr_center, STATE* sr)
 
         double taua = 1.0/rhoa;
         double taub = behind_state_specific_volume(rhoa,pa,pb);
+        double rhob = 1.0/taub;
         
         //M < 0 this case
         double M = -std::sqrt((pb - pa)/(taua - taub));
@@ -98,8 +103,9 @@ double RightCenteredWave(double Pslip, STATE* sr_center, STATE* sr)
         
         //save center state variables
         sr_center->u = ub;
-        sr_center->rho = 1.0/taub;
+        sr_center->rho = rhob;
         sr_center->p = pb;
+        sr_center->a = constant_state_soundspeed(rhob,pb);
 
         return ub;
     }
@@ -121,32 +127,29 @@ struct RP_Function
 };
 
 
+
 int main(int argc, char* argv[])
 {
     //Given left and right states sl and sr:
     double ul = 0.0;
     double rhol = 1.225;
     double pl = 100000;
+    double al = constant_state_soundspeed(rhol,pl);
 
     double ur = -1.0;
     double rhor = 1.1;
     double pr = 85000;
+    double ar = constant_state_soundspeed(rhor,pr);
 
-    STATE sl = {ul, rhol, pl};
-    STATE sl_c = {0, 0, 0};
-    STATE sr_c = {0, 0, 0};
-    STATE sr = {ur, rhor, pr};
+    STATE sl = {ul, rhol, pl, al};
+    STATE sl_c = {0, 0, 0, 0};
+    STATE sr_c = {0, 0, 0, 0};
+    STATE sr = {ur, rhor, pr, ar};
 
-    //Since velocities are equal on each side of the contact discontinuity, set
-    //    F(P_slip) = ul_star(P_slip) - ur-star(P_slip) = 0
-    
     //Need two initial guesses for P_slip to use secant method.
     //Just using pl and pr for now, seems better than previous method
     
-    //TODO: The order of passing pl and pr as initial guesses matters.
-    //      May need to consider velocity and density information in
-    //      making this decision. Try using entropy conditions and
-    //      properties of the waves
+    //TODO: check if below works better now that secant method bug fixed
     
         /*
         double frac_prange = 0.05*std::abs(pl-pr);
@@ -156,8 +159,8 @@ int main(int argc, char* argv[])
         printf("pg0 = %g\npg1 = %g\n\n",pg0,pg1);
         */
 
-    //Solve F(Pslip) = 0 using the secant method to
-    //find the pressure at the contact discontinuity
+    //Solve F(Pslip) = ul_star(P_slip) - ur-star(P_slip) = 0
+    //using the secant method to find the pressure at the contact discontinuity
     RP_Function F(&sl,&sl_c,&sr_c,&sr);
     double Pslip = secantMethod(F,pl,pr);
     //double Pslip = secantMethod(F,pg0,pg1);
@@ -169,50 +172,109 @@ int main(int argc, char* argv[])
     std::cout << "sr_c = " << sr_c;
     std::cout << "sr = " << sr;
 
-
     //TODO: make this a function and return a structure
     //      contain the riemann problem solution along with
     //      characteristic slopes seperating them
 
+    WAVETYPE LCW, RCW;
+    double left_shockspeed;
+    double left_trailing_fan_slope;
+    double left_leading_fan_slope;
+    double right_trailing_fan_slope;
+    double right_leading_fan_slope;
+    double right_shockspeed;
+
     if (Pslip > sl.p)
     {
-        double left_shockspeed =
+        LCW = WAVETYPE::SHOCK;
+        left_shockspeed =
             (sl.rho*sl.u - sl_c.rho*sl_c.u)/(sl.rho - sl_c.rho);
     }
     else
     {
+        LCW = WAVETYPE::SIMPLE;
         double al = constant_state_soundspeed(sl.rho,sl.p);
-        double left_trailing_fan_slope = sl.u - al;
+        left_trailing_fan_slope = sl.u - al;
 
         double al_c = constant_state_soundspeed(sl_c.rho,sl_c.p);
-        double left_leading_fan_slope = sl_c.u - al_c;
+        left_leading_fan_slope = sl_c.u - al_c;
     }
 
     double slip_slope = sl_c.u;
 
     if (Pslip < sr.p)
     {
+        RCW = WAVETYPE::SIMPLE;
         double ar_c = constant_state_soundspeed(sr_c.rho,sr_c.p);
-        double right_leading_fan_slope = sr_c.u - ar_c;
+        right_leading_fan_slope = sr_c.u - ar_c;
 
         double ar = constant_state_soundspeed(sr.rho,sr.p);
-        double right_trailing_fan_slope = sr.u - ar;
+        right_trailing_fan_slope = sr.u - ar;
     }
     else
     {
-        double right_shockspeed =
+        RCW = WAVETYPE::SHOCK;
+        right_shockspeed =
             (sr.rho*sr.u - sr_c.rho*sr_c.u)/(sr.rho - sr_c.rho);
     }
 
+
+    //TODO: Given any (x,t), compare ksi = x/t to the slopes
+    //      computed above to determine the solution
+    
     //sample (x,t) point 
     double x = 0.5;
     double t = 0.02;
     double ksi = x/t;
+    double u_riemann;
 
-    //TODO: Given any (x,t), compare ksi = x/t to the slopes
-    //      computed above to determine
+    if (ksi < slip_slope)
+    {
+        if (LCW == WAVETYPE::SHOCK)
+        {
+            if (ksi < left_shockspeed)
+                u_riemann = sl.u;
+            else
+                u_riemann = sl_c.u;
+        }
+        else
+        {
+            if (ksi < left_trailing_fan_slope)
+                u_riemann = sl.u;
+            else if (ksi < left_leading_fan_slope)
+            {
+                u_riemann = ((GAMMA-1.0)*sl.u + 2.0*(ksi + sl.a))/(GAMMA+1.0); 
+            }
+            else
+                u_riemann = sl_c.u;
+        }
+    }
+    else
+    {
+        if (RCW == WAVETYPE::SHOCK)
+        {
+            if (ksi < right_shockspeed)
+                u_riemann = sr_c.u;
+            else
+                u_riemann = sr.u;
 
-    //pg 11 of lecture_note4.pdf has comparisons 
+        }
+        else
+        {
+            if (ksi < right_leading_fan_slope)
+                u_riemann = sr_c.u;
+            else if (ksi < right_trailing_fan_slope)
+            {
+                u_riemann = ((GAMMA-1.0)*sr.u + 2.0*(ksi - sr.a))/(GAMMA+1.0); 
+            }
+            else
+                u_riemann = sr.u;
+
+        }
+    }
+
+    std::cout << "u_riemann = " << u_riemann << "\n";
 
     return 0;
 }
+
